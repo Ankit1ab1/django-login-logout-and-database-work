1b70ebde00bf7b0d9bc375f25ee33ec31210871e
Ankit1ab1 <52593151+Ankit1ab1@users.noreply.github.com>
2020-05-16 16:24:06 +0530

    Update settings.py

--- Loginproject/settings.py
+++ Loginproject/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -136,5 +135,4 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 LOGIN_URL='/basic_app/user_login'
 # Activate Django-Heroku.
-django_heroku.settings(locals())
 
